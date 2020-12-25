528bde4eabcc493bb4977d04877e364248afa69d
GGG KILLER <gggkiller2@gmail.com>
2020-12-25 20:46:15 -0300

    Remove unsafe algorithms and add async hashing methods.

--- Integrity/Util/Hash.cs
+++ Integrity/Util/Hash.cs
@@ -1,50 +1,21 @@
 ﻿using System;
+using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-
+using System.Threading;
+using System.Threading.Tasks;
+using Tsu.Numerics;
 using Crypto = System.Security.Cryptography;
 
 namespace Integrity.Util
 {
-    public static class Hash
+    /// <summary>
+    /// A hashing utility class.
+    /// </summary>
+    internal static class Hash
     {
-        public static String MD5 ( String data, Encoding encoding = null )
-        {
-            using var md5 = Crypto.MD5.Create ( );
-            return WithAlgorithm ( md5, data, encoding );
-        }
-
-        public static String MD5 ( Byte[] data )
-        {
-            using var md5 = Crypto.MD5.Create ( );
-            return WithAlgorithm ( md5, data );
-        }
-
-        public static String MD5 ( Stream data )
-        {
-            using var md5 = Crypto.MD5.Create ( );
-            return WithAlgorithm ( md5, data );
-        }
-
-        public static String SHA1 ( String data, Encoding encoding = null )
-        {
-            using var sha1 = Crypto.SHA1.Create ( );
-            return WithAlgorithm ( sha1, data, encoding );
-        }
-
-        public static String SHA1 ( Byte[] data )
-        {
-            using var sha1 = Crypto.SHA1.Create ( );
-            return WithAlgorithm ( sha1, data );
-        }
-
-        public static String SHA1 ( Stream data )
-        {
-            using var sha1 = Crypto.SHA1.Create ( );
-            return WithAlgorithm ( sha1, data );
-        }
-
         public static String SHA256 ( String data, Encoding encoding = null )
         {
             using var sha256 = Crypto.SHA256.Create ( );
@@ -61,6 +32,12 @@ namespace Integrity.Util
         {
             using var sha256 = Crypto.SHA256.Create ( );
             return WithAlgorithm ( sha256, data );
+        }
+
+        public static Task<String> SHA256Async ( Stream stream, Int32 bufferSize = 4 * FileSize.KiB, CancellationToken cancellationToken = default )
+        {
+            using var sha256 = Crypto.SHA256.Create ( );
+            return WithAlgorithmAsync ( sha256, stream, bufferSize, cancellationToken );
         }
 
         public static String SHA384 ( String data, Encoding encoding = null )
@@ -81,6 +58,12 @@ namespace Integrity.Util
             return WithAlgorithm ( sha384, data );
         }
 
+        public static Task<String> SHA384Async ( Stream stream, Int32 bufferSize = 4 * FileSize.KiB, CancellationToken cancellationToken = default )
+        {
+            using var sha384 = Crypto.SHA384.Create ( );
+            return WithAlgorithmAsync ( sha384, stream, bufferSize, cancellationToken );
+        }
+
         public static String SHA512 ( String data, Encoding encoding = null )
         {
             using var sha512 = Crypto.SHA512.Create ( );
@@ -99,34 +82,92 @@ namespace Integrity.Util
             return WithAlgorithm ( sha512, data );
         }
 
+        public static Task<String> SHA512Async ( Stream stream, Int32 bufferSize = 4 * FileSize.KiB, CancellationToken cancellationToken = default )
+        {
+            using var sha512 = Crypto.SHA512.Create ( );
+            return WithAlgorithmAsync ( sha512, stream, bufferSize, cancellationToken );
+        }
+
         public static String WithAlgorithm ( String name, String data, Encoding encoding = null )
         {
-            using var algo = HashAlgorithm.Create ( name.ToUpper ( ) );
+            if ( String.IsNullOrEmpty ( name ) )
+                throw new ArgumentException ( $"'{nameof ( name )}' cannot be null or empty.", nameof ( name ) );
+            if ( data is null )
+                throw new ArgumentNullException ( nameof ( data ) );
+
+            using var algo = HashAlgorithm.Create ( name.ToUpperInvariant ( ) );
             return WithAlgorithm ( algo, data, encoding );
         }
 
         public static String WithAlgorithm ( String name, Byte[] data )
         {
-            using var algo = HashAlgorithm.Create ( name.ToUpper ( ) );
+            if ( String.IsNullOrEmpty ( name ) )
+                throw new ArgumentException ( $"'{nameof ( name )}' cannot be null or empty.", nameof ( name ) );
+            if ( data is null )
+                throw new ArgumentNullException ( nameof ( data ) );
+
+            using var algo = HashAlgorithm.Create ( name.ToUpperInvariant ( ) );
             return WithAlgorithm ( algo, data );
         }
 
         public static String WithAlgorithm ( String name, Stream data )
         {
-            using var algo = HashAlgorithm.Create ( name.ToUpper ( ) );
+            if ( String.IsNullOrEmpty ( name ) )
+                throw new ArgumentException ( $"'{nameof ( name )}' cannot be null or empty.", nameof ( name ) );
+            if ( data is null )
+                throw new ArgumentNullException ( nameof ( data ) );
+
+            using var algo = HashAlgorithm.Create ( name.ToUpperInvariant ( ) );
             return WithAlgorithm ( algo, data );
         }
 
-        private static String WithAlgorithm ( HashAlgorithm algorithm, String data, Encoding encoding = null )
+        public static Task<String> WithAlgorithmAsync ( String name, Stream stream, Int32 bufferSize = 4 * FileSize.KiB, CancellationToken cancellationToken = default )
         {
-            encoding ??= Encoding.UTF8;
-            return WithAlgorithm ( algorithm, encoding.GetBytes ( data ) );
+            if ( String.IsNullOrEmpty ( name ) )
+                throw new ArgumentException ( $"'{nameof ( name )}' cannot be null or empty.", nameof ( name ) );
+            if ( stream is null )
+                throw new ArgumentNullException ( nameof ( stream ) );
+            if ( bufferSize < 1 )
+                throw new ArgumentOutOfRangeException ( nameof ( bufferSize ) );
+
+            using var algo = HashAlgorithm.Create ( name.ToUpperInvariant ( ) );
+            return WithAlgorithmAsync ( algo, stream, bufferSize, cancellationToken );
         }
 
+        private static String WithAlgorithm ( HashAlgorithm algorithm, String data, Encoding encoding = null ) =>
+            WithAlgorithm ( algorithm, ( encoding ?? Encoding.Default ).GetBytes ( data ) );
+
         private static String WithAlgorithm ( HashAlgorithm algorithm, Byte[] data ) =>
-            String.Concat ( Array.ConvertAll ( algorithm.ComputeHash ( data ), b => $"{b:x2}" ) );
+            ByteArrayToHex ( algorithm.ComputeHash ( data ) );
 
         private static String WithAlgorithm ( HashAlgorithm algorithm, Stream data ) =>
-            String.Concat ( Array.ConvertAll ( algorithm.ComputeHash ( data ), b => $"{b:x2}" ) );
+            ByteArrayToHex ( algorithm.ComputeHash ( data ) );
+
+        [SuppressMessage ( "Performance", "CA1835:Prefer the 'Memory'-based overloads for 'ReadAsync' and 'WriteAsync'", Justification = "Can't use Memory<T> with HashAlgorithm." )]
+        private static async Task<String> WithAlgorithmAsync ( HashAlgorithm algorithm, Stream stream, Int32 bufferSize = 4 * FileSize.KiB, CancellationToken cancellationToken = default )
+        {
+            if ( stream is null )
+                throw new ArgumentNullException ( nameof ( stream ) );
+
+            var buffer = ArrayPool<Byte>.Shared.Rent ( bufferSize );
+            try
+            {
+                var length = 0;
+                while ( ( length = await stream.ReadAsync ( buffer, 0, bufferSize, cancellationToken ).ConfigureAwait ( false ) ) > 0 )
+                {
+                    algorithm.TransformBlock ( buffer, 0, length, null, 0 );
+                }
+                algorithm.TransformFinalBlock ( buffer, 0, 0 );
+
+                return ByteArrayToHex ( algorithm.Hash );
+            }
+            finally
+            {
+                ArrayPool<Byte>.Shared.Return ( buffer );
+            }
+        }
+
+        private static String ByteArrayToHex ( Byte[] array ) =>
+            String.Concat ( Array.ConvertAll ( array, b => $"{b:x2}" ) );
     }
 }
